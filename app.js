831f91548a5e4e73e5fdc6a34b85977898e9a196
pankaj021 <kssm.pankaj@gmail.com>
2020-05-06 23:06:41 +0530

    request parsing

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json())
 
 app.use((req, res, next) => {
   console.log("inspecting request.....");
+  console.log("req.method::::::::::::::::::", req.method);
   console.log("req.url::::::::::::::::::", req.url);
+  console.log("req.query::::::::::::::::::", req.query);
   console.log("req.body:::::::::::::::::", req.body);
   next()
 })
