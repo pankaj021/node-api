5821b5e78e1d45cc136b791bcf22e353adcbfcdf
pankaj021 <kssm.pankaj@gmail.com>
2020-05-06 22:53:29 +0530

    req and res basic

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  console.log("middleware 1 called..........");
-  next(new Error("Went wrong in middleware2"))
+  console.log("inspecting request.....");
+  console.log("req.url::::::::::::::::::", req.url);
+  console.log("req.body:::::::::::::::::", req.body);
+  next()
 })
 
 app.use((req, res, next) => {
-  console.log("middleware 2 called..........");
+  console.log("responding to client request.....");
+  res.send("SUCCESS RESPONSE");
 })
 
 app.use((err, req, res, next) => {
-  console.log("middleware 3 called..........", err);
+  console.log("Error middleware, since it has 4 params::::", err);
 })
 
 module.exports = app;
